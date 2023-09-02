f6dfaa2c362804f7c1e7209daae250ed7e195a2c
Cui <1742618460@qq.com>
2023-09-02 15:42:13 +0800

    Fixed bug

--- leapYear.java
+++ leapYear.java
@@ -10,7 +10,7 @@ public class leapYear{
                
                 //if year is divisible by 4, it is a leap year
                
-                if((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)))
+                if(（(year % 400 == 0) || ((year % 4 == 0)） && (year % 100 != 0)))
                         System.out.println("Year " + year + " is a leap year");
                 else
                         System.out.println("Year " + year + " is not a leap year");
